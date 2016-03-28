62e10e1416892211347f685ca6dafe98c59f6890
alexui <alex.budau@yahoo.com>
2016-03-28 17:22:33 +0300

    lab3 resolved

---
+++ PhoneDialer/app/src/main/java/com/politehnica/alexbudau/phonedialer/Constants.java
@@ -0,0 +1,20 @@
+package com.politehnica.alexbudau.phonedialer;
+
+/**
+ * Created by Alex on 3/28/2016.
+ */
+public class Constants {
+
+    public final static int [] phoneDialerButtons = {
+            R.id.button0,
+            R.id.button1,
+            R.id.button2,
+            R.id.button3,
+            R.id.button4,
+            R.id.button5,
+            R.id.button6,
+            R.id.button7,
+            R.id.button8,
+            R.id.button9
+    };
+}
